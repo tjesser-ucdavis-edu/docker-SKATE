81051d98a61232e67a095aeb17f6a80115f10d99
tjesser <tjesser@ucdavis.edu>
2019-01-31 17:03:44 -0800

    Stop leftover running containers before running new containers

--- run-seismo-app.sh
+++ run-seismo-app.sh
@@ -9,6 +9,14 @@ then
   docker network create seismo-app
 fi
 
+docker ps \
+  --filter 'name=seismo-database-run' \
+  --filter 'name=seismo-server-run' \
+  --filter 'name=seismo-client-run' \
+  --quiet \
+| xargs \
+  docker stop
+
 docker run \
   --detach \
   --name seismo-database-run \
